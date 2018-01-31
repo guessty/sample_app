221e5c115f718d46ad191379f8a5532dee8cb27a
Christian <chistian.guest@outlook.com>
2018-01-31 09:25:57 +0000

    Finish User microposts

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -37,7 +37,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test "micropost sidebar count" do
     log_in_as(@user)
     get root_path
-    assert_match "#{FILL_IN} microposts", response.body
+    assert_match "34 microposts", response.body
     # User with zero microposts
     other_user = users(:malory)
     log_in_as(other_user)
@@ -45,7 +45,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_match "0 microposts", response.body
     other_user.microposts.create!(content: "A micropost")
     get root_path
-    assert_match FILL_IN, response.body
+    assert_match "1 micropost", response.body
   end
 
 end
