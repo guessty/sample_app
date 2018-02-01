7d3d2c41a42defdaea39c980febaff2ae6e88c37
Christian <chistian.guest@outlook.com>
2018-02-01 09:18:27 +0000

    Fixed failing test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -32,16 +32,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
   
-  # test "should not allow the admin attribute to be edited via the web" do
-  #   log_in_as(@other_user)
-  #   assert_not @other_user.admin?
-  #   patch user_path(@other_user), params: {
-  #                                   user: { password:              FILL_IN,
-  #                                           password_confirmation: FILL_IN,
-  #                                           admin: FILL_IN } }
-  #   assert_not @other_user.FILL_IN.admin?
-  # end
-  
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
@@ -61,6 +51,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       delete user_path(@user)
     end
+    get following_user_path(@user)
     assert_redirected_to login_url
   end
 
